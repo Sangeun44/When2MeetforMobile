f743f78e1e87cb99905529d7ecb0dfa52702aef6
eviebail <ebail@seas.upenn.edu>
2018-02-13 16:39:40 -0500

    Add files via upload
    Added HomeScreenActivity.java that handles the Home page, and activity_home.xml that has the home page layout so far. Updated AndroidManifest.xml to include HomeScreenActivity.

---
+++ HomeScreenActivity.java
@@ -0,0 +1,18 @@
+package edu.upenn.cis350.g8.when2meetformobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Evie on 2/12/18.
+ */
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
