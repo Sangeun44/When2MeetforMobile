155771cf125f3e53a8b03600cf5c500adcc08958
saniyah-shaikh <saniyah@seas.upenn.edu>
2018-02-13 01:02:50 -0500

    added layout for session page

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/JoinedSessionDisplayActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/JoinedSessionDisplayActivity.java
@@ -1,7 +1,10 @@
 package edu.upenn.cis350.g8.when2meetformobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class JoinedSessionDisplayActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class JoinedSessionDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joined_session_display);
+    }
+
+    public void onHomeButtonClick(View v) {
+        Intent i = new Intent(this, JoinedSessionsActivity.class);
+        setResult(RESULT_OK, i);
+        finish();
+    }
+
+    public void onEnterTimesButtonClick(View v) {
+        Toast.makeText(getApplicationContext(), "Going to Enter Times Page...",
+                Toast.LENGTH_SHORT).show();
     }
 }
