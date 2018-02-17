d8ca5e9bcdbb4c563bb648eeec9df3447b1ff8a2
marsalad <buggymarsala@gmail.com>
2018-02-16 21:27:42 -0500

    logout button

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/MainActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/MainActivity.java
@@ -1,11 +1,12 @@
 package edu.upenn.cis350.g8.when2meetformobile;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -13,33 +14,63 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "When2MeetMain";
     private static final int RC_SIGN_IN = 123;
     private GoogleSignInAccount account;
     private SignInButton signInButton;
+    private ImageButton signOutButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        account = GoogleSignIn.getLastSignedInAccount(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        db = FirebaseFirestore.getInstance();
+
         signInButton = findViewById(R.id.sign_in_button);
+        signOutButton = findViewById(R.id.sign_out_button);
         signInButton.setOnClickListener(this);
+        signOutButton.setOnClickListener(this);
+
+        account = GoogleSignIn.getLastSignedInAccount(this);
         updateUI();
     }
 
     @Override
     public void onClick(View view) {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        if (view.getId() == R.id.sign_in_button) {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        } else if (view.getId() == R.id.sign_out_button) {
+            mGoogleSignInClient.signOut()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            account = null;
+                            updateUI();
+                        }
+                    });
+        }
     }
 
     @Override
@@ -49,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 account = task.getResult(ApiException.class);
+                addUserToDb();
+                Log.d(TAG, "signInResult:success");
             } catch (ApiException e) {
                 Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
             }
@@ -56,9 +89,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         updateUI();
     }
 
-    private void updateUI() {
-        signInButton.setVisibility(account == null ? View.VISIBLE : View.INVISIBLE);
+    private void addUserToDb() {
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("name",account.getDisplayName());
+        userData.put("meetings", new ArrayList<DocumentReference>());
+        db.collection("users").document(account.getId())
+                .set(userData, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 
-
+    private void updateUI() {
+        signInButton.setVisibility(account == null ? View.VISIBLE : View.INVISIBLE);
+        signOutButton.setVisibility(account == null ? View.INVISIBLE : View.VISIBLE);
+    }
 }
