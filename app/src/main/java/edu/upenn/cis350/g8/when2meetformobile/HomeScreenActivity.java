1d10b38a8338c591dfd5a355f3d32663da17a555
eviebail <ebail@seas.upenn.edu>
2018-02-18 16:24:42 -0500

    Add files via upload
    Final Iteration 1 version of the Home Screen

---
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/HomeScreenActivity.java
@@ -0,0 +1,52 @@
+package edu.upenn.cis350.g8.when2meetformobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Evie on 2/12/18.
+ */
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void updateNotifications (String message) {
+        TextView text = findViewById(R.id.notifications);
+        text.append(message);
+    }
+
+    public void onMySessionsButtonClick(View view) {
+
+    }
+
+    public void onJoinedSessionsButtonClick(View view) {
+
+    }
+
+    public void onLogoutButtonClick(View view) {
+        //return to login page
+        finish();
+    }
+
+    public void onCreateButtonClick(View view) {
+        //Sang's page
+        //CreateSessionActivity
+    }
+
+    public void onProfileButtonClick(View view) {
+
+    }
+
+    public void onJoinButtonClick(View view) {
+
+    }
+
+}
