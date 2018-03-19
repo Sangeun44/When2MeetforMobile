2b653c36b3cdd8e350265669eda27eaf0f782823
eviebail <ebail@seas.upenn.edu>
2018-03-19 19:14:37 -0400

    Add files via upload
    updated EnterTimes. most recent version

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/EnterTimesActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/EnterTimesActivity.java
@@ -1,27 +1,35 @@
 package edu.upenn.cis350.g8.when2meetformobile;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Evie on 3/9/18.
@@ -32,27 +40,22 @@ public class EnterTimesActivity extends AppCompatActivity {
     private static final String TAG = "When2MeetJoinedSessions";
     private List<Meeting> meetings;
     private String currentSession;
+    private String userId = "";
+    List<String> days;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO: need to make sure we pass this from joinedSessionDisplayActivity
-        int userID = getIntent().getIntExtra("accountName", 0);
-        populateMap(userID);
+        userId = getIntent().getStringExtra("accountName");
+        getMeetings();
         currentSession = getIntent().getStringExtra("MEETING");
-
-        //TODO: Make a view that dynamically loads the correct spinners based on high, low, dates
         setContentView(R.layout.activity_enter_times);
         loadDates();
         loadSpinners();
-
-        //TODO: Implement plus button functionality to add more times
-
-        //TODO: Implement a way to save this information into a reasonable format
     }
 
-    private void populateMap(int ownerID) {
-        FirebaseFirestore.getInstance().collection("meetings").whereEqualTo("owner", ownerID).get()
+    private void getMeetings() {
+        FirebaseFirestore.getInstance().collection("meetings").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot documentSnapshots) {
@@ -76,13 +79,14 @@ public class EnterTimesActivity extends AppCompatActivity {
         //finds the meeting that was clicked on, and loads the dates
         for (Meeting m : meetings) {
             if (m.getName().equals(currentSession)) {
-                List<String> days = m.getDates();
+                days = m.getDates();
                 for (String day : days) {
                     // from Stack Exchange
                     LinearLayout myLayout = findViewById(R.id.datesBar);
                     TextView date = new TextView(this);
+                    date.setText(day);
                     date.setLayoutParams(new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            205,
                             LinearLayout.LayoutParams.MATCH_PARENT));
                     myLayout.addView(date);
                 }
@@ -98,14 +102,19 @@ public class EnterTimesActivity extends AppCompatActivity {
 
                 for (int i = 0; i < numOptions; i++) {
                     LinearLayout myLayout = findViewById(R.id.selectorBar);
-                    Spinner timesSelector = new Spinner(this);
-                    //adapted from Stack Exchange
-                    //TODO: Figure out the format of hi/lo times
+                    //layout for new selector/plus button pair
+                    LinearLayout child = new LinearLayout(this);
+                    child.setOrientation(LinearLayout.VERTICAL);
+                    child.setLayoutParams(new LinearLayout.LayoutParams(
+                            200,
+                            LinearLayout.LayoutParams.MATCH_PARENT));
+                    child.setId(i);
 
+                    //create spinner with times
                     //set the time interval array for all the spinners
-                    List<String> spinnerArray =  new ArrayList<>();
+                    List<String> spinnerArray = new ArrayList<>();
 
-                    for (int j = 0; j < m.getHigh_time() - m.getLow_time(); j+=100) {
+                    for (int j = 0; j < m.getHigh_time() - m.getLow_time(); j+=1) {
                         StringBuilder time = new StringBuilder();
                         time.append(m.getLow_time() + j);
                         spinnerArray.add(time.toString());
@@ -115,54 +124,188 @@ public class EnterTimesActivity extends AppCompatActivity {
                     t.append(m.getHigh_time());
                     spinnerArray.add(t.toString());
 
-                    //display the spinner
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                            this, android.R.layout.simple_spinner_item, spinnerArray);
+                    Spinner start = createSelector(spinnerArray, "START");
+                    //set the time interval array for all the spinners
 
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    timesSelector.setAdapter(adapter);
+                    List<String> spinnerArr = new ArrayList<>();
 
-                    timesSelector.setLayoutParams(new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.MATCH_PARENT));
-                    myLayout.addView(timesSelector);
+                    for (int j = 0; j < m.getHigh_time() - m.getLow_time(); j+=1) {
+                        StringBuilder time = new StringBuilder();
+                        time.append(m.getLow_time() + j);
+                        spinnerArr.add(time.toString());
+                    }
+
+                    StringBuilder r = new StringBuilder();
+                    r.append(m.getHigh_time());
+                    spinnerArr.add(r.toString());
+
+                    Spinner end = createSelector(spinnerArray, "END");
+
+                    child.addView(start);
+                    child.addView(end);
+
+                    //checkbox
+                    CheckBox preferred = new CheckBox(this);
+                    LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(150, 150);
+                    layoutParams.gravity= Gravity.CENTER;
+                    preferred.setLayoutParams(layoutParams);
+                    child.addView(preferred);
+
+                    ImageButton btn = createPlus(i);
+                    child.addView(btn);
+                    myLayout.addView(child);
                 }
             }
         }
     }
 
-    public void loadPlus() {
+    private Spinner createSelector(List<String> values, String which) {
+        Spinner timesSelector = new Spinner(this);
+        values.add(0, which);
+        //display the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, values);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timesSelector.setAdapter(adapter);
+
+        timesSelector.setLayoutParams(new LinearLayout.LayoutParams(
+                400,
+                200));
+        return timesSelector;
+    }
+
+    public ImageButton createPlus(int ID) {
         //finds the meeting that was clicked on, and loads the dates
+        final ImageButton plus = new ImageButton(this);
+        plus.setImageResource(R.drawable.plus);
+        plus.setId(ID*100);
+        //add more selectors when pressed
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                @SuppressLint("ResourceType") int ident = plus.getId() / 100;
+                addSelector(ident);
+            }
+        });
+
+        plus.setLayoutParams(new LinearLayout.LayoutParams(
+                200,
+                200));
+
+        return plus;
+    }
+
+    private void addSelector(int ID) {
+        LinearLayout myLayout = findViewById(ID);
+
+        //create the selector
         for (Meeting m : meetings) {
             if (m.getName().equals(currentSession)) {
-                List<String> days = m.getDates();
-                for (int k = 0; k < m.getDates().size(); k++) {
-                    // from Stack Exchange
-                    LinearLayout myLayout = findViewById(R.id.plusBar);
-                    final ImageButton plus = new ImageButton(this);
-                    plus.setImageResource(R.drawable.plus);
-                    plus.setId(k);
-                    //add more selectors when pressed
-                    plus.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            //TODO: Add new selector in the right position
-                            addSelector(plus.getId());
-                        }
-                    });
 
-                    plus.setLayoutParams(new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.MATCH_PARENT));
+                List<String> spinnerArray = new ArrayList<>();
 
-                    myLayout.addView(plus);
+                for (int j = 0; j < m.getHigh_time() - m.getLow_time(); j+=1) {
+                    StringBuilder time = new StringBuilder();
+                    time.append(m.getLow_time() + j);
+                    spinnerArray.add(time.toString());
                 }
-            }
+
+                StringBuilder t = new StringBuilder();
+                t.append(m.getHigh_time());
+                spinnerArray.add(t.toString());
+
+                Spinner start = createSelector(spinnerArray, "START");
+                //set the time interval array for all the spinners
+
+                List<String> spinnerArr = new ArrayList<>();
+
+                for (int j = 0; j < m.getHigh_time() - m.getLow_time(); j+=1) {
+                    StringBuilder time = new StringBuilder();
+                    time.append(m.getLow_time() + j);
+                    spinnerArr.add(time.toString());
+                }
+
+                StringBuilder r = new StringBuilder();
+                r.append(m.getHigh_time());
+                spinnerArr.add(r.toString());
+
+                Spinner end = createSelector(spinnerArray, "END");
+
+                myLayout.addView(start, myLayout.getChildCount() - 1);
+                myLayout.addView(end, myLayout.getChildCount() - 1);
+
+                //checkbox
+                CheckBox preferred = new CheckBox(this);
+                LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(150, 150);
+                layoutParams.gravity= Gravity.CENTER;
+                preferred.setLayoutParams(layoutParams);
+                myLayout.addView(preferred, myLayout.getChildCount() - 1);
+                }
         }
+
     }
 
-    public void addSelector(int ID) {
-        //TODO: Implement this
+    public void onEnterClick(View view) {
+        String startTime, endTime;
+        LinearLayout selectorBar = findViewById(R.id.selectorBar);
+        for (int i = 0; i < selectorBar.getChildCount(); i++) {
+            List<String> enteredTimes = new ArrayList<String>();
+            LinearLayout column = (LinearLayout) selectorBar.getChildAt(i);
+            for (int j = 0 ; j < column.getChildCount() - 2; j+=3) {
+                if (column.getChildAt(j) instanceof Spinner) {
+                    Spinner start = (Spinner) column.getChildAt(j);
+                    if (column.getChildAt(j + 1) instanceof Spinner) {
+                        Spinner end = (Spinner) column.getChildAt(j);
+                        startTime = start.getSelectedItem().toString();
+                        endTime = end.getSelectedItem().toString();
+                        if (column.getChildAt(j + 2) instanceof CheckBox) {
+                            //TODO: Use this data to implement preferred times functionality
+                            CheckBox check = (CheckBox) column.getChildAt(j + 2);
+                            if (check.isChecked()) { } else { }
+                        }
+
+                        enteredTimes.add(startTime);
+                        enteredTimes.add(endTime);
+
+                        if (startTime.compareTo(endTime) > 0) {
+                            Toast.makeText(this, "Start times can not be greater" +
+                                            " than end times. Please fix it and resubmit.",
+                                    Toast.LENGTH_SHORT).show();
+                            break;
+                        } else if (startTime.equals("START") ||
+                                endTime.equals("END")) {
+                            Toast.makeText(this, "Please fill in available times" +
+                                            " before submitting.",
+                                    Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                    }
+                }
+            }
+            Map<String, List<String>> values = new HashMap<>();
+            values.put(days.get(i), enteredTimes);
+            updateDB(values);
+        }
+        finish();
+    }
+
+    private void updateDB(Map<String, List<String>> data) {
+        FirebaseFirestore.getInstance().collection("meetings").document(currentSession)
+                .collection("users").document(userId)
+                .set(data, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 
 }
