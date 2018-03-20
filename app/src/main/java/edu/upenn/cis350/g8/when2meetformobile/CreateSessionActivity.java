c7079532cef7fc3025a35268c724a6f25f5868e8
marsalad <buggymarsala@gmail.com>
2018-03-20 06:26:42 -0400

    create and display random code

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/CreateSessionActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/CreateSessionActivity.java
@@ -1,7 +1,5 @@
 package edu.upenn.cis350.g8.when2meetformobile;
 
-import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
@@ -21,7 +19,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.text.SimpleDateFormat;
@@ -30,241 +27,130 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-
-/**
- * Created by Sang on 2/15/18.
- */
 
 public class CreateSessionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    final static int createSession_ID = 1; //main id
     final static String TAG = "CreateSessionAct";
-    private Spinner mode; //dates or days
-    private Spinner earliest;
-    private Spinner latest;
-    private Spinner type; //code or email
 
-    //string storage
-    private String modeStr;
-    private String earliestStr;
-    private String latestStr;
-    private String typeStr;
-    private String meetingIDStr;
-    private String eventName;
+    FirebaseFirestore database;
+    DocumentReference ref;
 
-    //storage
     ArrayList<String> daysSelected;
     ArrayList<String> datesSelected;
-    ArrayList<String> emailList;
-
-    private List<Meeting> myMeetings = new ArrayList<Meeting>();
-    private Meeting meet;
-
-    public CreateSessionActivity() {
-        daysSelected = new ArrayList<String>();
-        datesSelected = new ArrayList<String>();
-        emailList = new ArrayList<String>();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createsession);
 
-        //populate 4 spinners
-        //mode
-        populateMode();
-        //earliest
-        populateEarliest();
-        //latest
-        populateLatest();
-        //type
-        populateType();
+        database = FirebaseFirestore.getInstance();
+        ref = database.collection("my_collection").document();
+        ((TextView) findViewById(R.id.code)).setText(ref.getId());
 
-        //same onItemSelected
-        mode.setOnItemSelectedListener(this);
-        earliest.setOnItemSelectedListener(this);
-        latest.setOnItemSelectedListener(this);
-        type.setOnItemSelectedListener(this);
+        daysSelected = new ArrayList<>();
+        datesSelected = new ArrayList<>();
+        initDates();
 
-        //event name
-        EditText text = (EditText)findViewById(R.id.eventName);
-        eventName = (String) text.toString();
-
-        //set up dates
-        setUpDates();
+        initSpinner((Spinner) findViewById(R.id.mode), R.array.mode);
+        initSpinner((Spinner) findViewById(R.id.earliest), R.array.hours);
+        initSpinner((Spinner) findViewById(R.id.latest), R.array.hours);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // for each spinner and their relative choices
-        switch(parent.getId()) {
-            case R.id.mode:
-                //mode = date or days
-                modeStr = mode.getSelectedItem().toString();
-                View week = findViewById(R.id.weekdays);
-                View month = findViewById(R.id.month);
-                if(modeStr.equals("Specific Date")) {
-                    week.setVisibility(View.INVISIBLE);
-                    month.setVisibility(View.VISIBLE);
-                }
-                else {
-                    week.setVisibility(View.VISIBLE);
-                    month.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case R.id.earliest:
-                //earliest time
-                earliestStr = earliest.getSelectedItem().toString();
-                break;
-            case R.id.latest:
-                //latest time
-                latestStr = latest.getSelectedItem().toString();
-                break;
-            case R.id.choice:
-                //email or code?
-                typeStr = type.getSelectedItem().toString();
-                View codeV = findViewById(R.id.code);
-                View codedV = findViewById(R.id.coded);
-                if(typeStr.equals("code")) {
-                    //code
-                    //codeV.setVisibility(View.VISIBLE);
-//                    //codedV.setVisibility(View.VISIBLE);
-//                    String code = createRandomCode();
-//                    ((TextView) codedV).setText(code);
-//                    meetingIDStr = code;
-                }
-                else {
-                    //email
-                    codeV.setVisibility(View.INVISIBLE);
-                    codedV.setVisibility(View.INVISIBLE);
-                    startEmailActivity(); //start email activity when user chooses email
-                }
-                break;
+    private void initDates() {
+        for(int i = 0; i < 35; i++) {
+            Date dt = new Date();
+            Calendar c = Calendar.getInstance();
+            c.setTime(dt);
+            c.add(Calendar.DATE, i);
+            dt = c.getTime();
+            String date = new SimpleDateFormat("MM/dd").format(dt);
+
+            int id = getResources().getIdentifier("btn" + i, "id", getPackageName());
+            ((Button) findViewById(id)).setText(date);
         }
     }
 
-    private String createRandomCode() {
-        return "code";
-    }
-
-    //start emailActivity to insert emails when user chooses email
-    public void startEmailActivity() {
-        Intent i = new Intent(this, EmailActivity.class);
-        startActivityForResult(i, createSession_ID);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Check which request we're responding to
-        if (requestCode == createSession_ID) {
-            // Make sure the request was successful
-            if (resultCode == RESULT_OK) {
-                emailList = data.getStringArrayListExtra("result");
-            }
-        }
-    }
-
-    //if nothing is selected 4 spinners
-    public void onNothingSelected(AdapterView<?> arg0) {
-        //do nothing
+    private void initSpinner(Spinner spinner, int arrayId) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                arrayId, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     //select weekdays
     public void onSelectWeekdays(View view) {
-
-        Button n = (Button) view;
-        String name = n.getText().toString();
-        if(daysSelected.contains(name)){
+        String name = ((Button) view).getText().toString();
+        if (daysSelected.contains(name)) {
             daysSelected.remove(name);
-            view.getBackground().setColorFilter(Color.parseColor("#66999999"), PorterDuff.Mode.DARKEN);
-        }
-        else {
+            view.getBackground().setColorFilter(Color.parseColor("#66999999"),
+                    PorterDuff.Mode.DARKEN);
+        } else {
             daysSelected.add(name);
-            view.getBackground().setColorFilter(Color.parseColor("#9900ff00"), PorterDuff.Mode.DARKEN);
+            view.getBackground().setColorFilter(Color.parseColor("#9900ff00"),
+                    PorterDuff.Mode.DARKEN);
         }
     }
 
     //select dates
     public void onSelectDates(View view) {
-        Date dt = new Date();
-        String monthDay = ((Button)view).getText().toString();
-        int year = Calendar.getInstance().get(Calendar.YEAR);
-        String monthDayYear = monthDay + "/" + year;
+        String date = ((Button) view).getText().toString() + "/" +
+                Calendar.getInstance().get(Calendar.YEAR);
 
-        if(datesSelected.contains(monthDayYear)) {
-            datesSelected.remove(monthDayYear);
-            view.getBackground().setColorFilter(Color.parseColor("#66999999"), PorterDuff.Mode.DARKEN);
-        }
-        else {
-            datesSelected.add(monthDayYear);
-            view.getBackground().setColorFilter(Color.parseColor("#9900ff00"), PorterDuff.Mode.DARKEN);
+        if (datesSelected.contains(date)) {
+            datesSelected.remove(date);
+            view.getBackground().setColorFilter(Color.parseColor("#66999999"),
+                    PorterDuff.Mode.DARKEN);
+        } else {
+            datesSelected.add(date);
+            view.getBackground().setColorFilter(Color.parseColor("#9900ff00"),
+                    PorterDuff.Mode.DARKEN);
         }
         Collections.sort(datesSelected);
     }
 
     //clicked on the create event button
     public void onClickCreateButton(View view) {
+        String eventName = ((EditText) findViewById(R.id.eventName)).getText().toString();
+        String modeStr = ((Spinner) findViewById(R.id.mode)).getSelectedItem().toString();
+        int t1 = changeTime(((Spinner) findViewById(R.id.earliest)).getSelectedItem().toString());
+        int t2 = changeTime(((Spinner) findViewById(R.id.latest)).getSelectedItem().toString());
 
-        //check event name
-        EditText eventN = (EditText) findViewById(R.id.eventName);
-        if (eventN.getText().toString().length() == 0){
+        if (eventName.length() == 0) {
             Toast.makeText(CreateSessionActivity.this,
-                    "Remember to enter in the event name!",
-                    Toast.LENGTH_SHORT).show();
-        } else if(modeStr.equals("Specific Date") && datesSelected.isEmpty()) {
+                    "Remember to enter in the event name!", Toast.LENGTH_SHORT).show();
+        } else if (modeStr.equals("Specific Date") && datesSelected.isEmpty()) {
             Toast.makeText(CreateSessionActivity.this,
-                    "Remember to select dates!",
-                    Toast.LENGTH_SHORT).show();
-        } else if(modeStr.equals("Days of the Week") && daysSelected.isEmpty()) {
-            //check number of days selected if
+                    "Remember to select dates!", Toast.LENGTH_SHORT).show();
+        } else if (modeStr.equals("Days of the Week") && daysSelected.isEmpty()) {
             Toast.makeText(CreateSessionActivity.this,
-                    "Remember to select days!",
-                    Toast.LENGTH_SHORT).show();
-        } else if(typeStr.equals("email") && emailList.isEmpty() || emailList.size() < 0) {
+                    "Remember to select days!", Toast.LENGTH_SHORT).show();
+        } else if (t1 >= t2) {
             Toast.makeText(CreateSessionActivity.this,
-                    "Remember to enter in emails!",
-                    Toast.LENGTH_SHORT).show();
-        } else if(typeStr.equals("email") && emailList.isEmpty() || emailList.size() > 0) {
-            Toast.makeText(CreateSessionActivity.this,
-                    "Now send the emails",
-                    Toast.LENGTH_SHORT).show();
-            eventName = eventN.getText().toString();
-            updateDB();
-            sendEmail();
-
-            finish();
-
-        }
-        else {
-            eventName = eventN.getText().toString();
-            updateDB();
+                    "Remember to choose valid times!", Toast.LENGTH_SHORT).show();
+        } else {
+            updateDB(eventName, t1, t2);
             finish();
         }
     }
 
-    public void updateDB() {
+    private int changeTime(String time) {
+        int t = Integer.parseInt(time.substring(0,2).trim());
+        if (time.contains("pm") && t < 12) {
+            return t + 12;
+        } else if(time.contains("am") && t == 12) {
+            return t + 12;
+        } else {
+            return t;
+        }
+    }
 
-        int time1 = changeTime(earliestStr);
-        int time2 = changeTime(latestStr);
-        int high_time = Math.max(time1, time2);
-        int low_time = Math.min(time1, time2);
-
-        Intent i = getIntent();
-        String owner_id = i.getStringExtra("accountKey");
-
-        HashMap<String, User> users = new HashMap<String, User>();
-
-        meet = new Meeting(users, datesSelected, high_time, low_time, eventName, owner_id);
+    private void updateDB(String eventName, int t1, int t2) {
+        String owner_id = getIntent().getStringExtra("accountKey");
+        HashMap<String, User> users = new HashMap<>();
+        Meeting meet = new Meeting(users, datesSelected, t2, t1, eventName, owner_id);
         meet.addUsers(owner_id);
 
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        DocumentReference ref = database.collection("my_collection").document();
-        FirebaseFirestore.getInstance().collection("meetings").document(ref.getId())
+        database.collection("meetings").document(ref.getId())
                 .set(meet, SetOptions.merge())
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -278,122 +164,17 @@ public class CreateSessionActivity extends AppCompatActivity implements AdapterV
                         Log.w(TAG, "Error writing document", e);
                     }
                 });
-
-        meetingIDStr = ref.getId();
-        Toast.makeText(CreateSessionActivity.this,
-                "Meeting ID:" + ref.getId(),
-                Toast.LENGTH_LONG).show();
     }
 
-    public int changeTime(String time) {
-        int t = Integer.parseInt(time.substring(0,2).trim());
-        if(time.contains("pm") && t < 12) {
-            return t + 12;
-        }
-        else if(time.contains("am") && t == 12) {
-            return t + 12;
-        }
-        return t;
-    }
-
-    //send emails to the email list with the meeting id
-    public void sendEmail() {
-        String[] emails = emailList.toArray(new String[emailList.size()]);
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.setType("message/rfc822");
-        i.putExtra(Intent.EXTRA_EMAIL  , emails);
-        i.putExtra(Intent.EXTRA_SUBJECT, "When2Meet Session");
-        i.putExtra(Intent.EXTRA_TEXT   , "Please check your invitation to fill out the new When2Meet \n" + "Meeting code: " + meetingIDStr);
-        try {
-            startActivity(Intent.createChooser(i, "Send mail..."));
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(CreateSessionActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (adapterView.getId() == R.id.mode) {
+            boolean isDate = adapterView.getSelectedItem().toString().equals("Specific Date");
+            findViewById(R.id.weekdays).setVisibility(isDate ? View.INVISIBLE : View.VISIBLE);
+            findViewById(R.id.month).setVisibility(isDate ? View.VISIBLE : View.INVISIBLE);
         }
     }
 
-//    //Random code making for code sending
-//    public String createRandomCode() {
-//        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-//
-//        StringBuilder build = new StringBuilder();
-//        while(build.toString().length() < 4) {
-//            Random rand = new Random();
-//            int value = rand.nextInt(36);
-//            char c = str.charAt(value);
-//            build.append(c);
-//        }
-//        return build.toString();
-//    }
-
-    //set up dates
-    public void setUpDates() {
-        for(int i = 0; i < 35; i++) {
-            Date dt = new Date();
-            SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd");
-
-            Calendar c = Calendar.getInstance();
-            c.setTime(dt);
-            c.add(Calendar.DATE, i);
-            dt = c.getTime();
-            String date = DATE_FORMAT.format(dt);
-
-            Resources res = getResources();
-            String name = "btn" + i;
-            int id = res.getIdentifier(name, "id", this.getPackageName());
-            Button btn = findViewById(id);
-            btn.setText(date.toString());
-        }
-    }
-
-///--------spinner fill
-
-    //fill mode
-    public void populateMode() {
-        mode = (Spinner) findViewById(R.id.mode);
-
-        //Create an ArrAdapt using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.mode, android.R.layout.simple_spinner_item);
-        //Specify the layout to use when the list of choices appears.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        mode.setAdapter(adapter);
-    }
-
-    //fill earliest
-    public void populateEarliest() {
-        earliest = (Spinner) findViewById(R.id.earliest);
-
-        //Create an ArrAdapt using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.hours, android.R.layout.simple_spinner_item);
-        //Specify the layout to use when the list of choices appears.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        earliest.setAdapter(adapter);
-    }
-
-    //fill latest
-    public void populateLatest() {
-        latest = (Spinner) findViewById(R.id.latest);
-        //Create an ArrAdapt using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.hours, android.R.layout.simple_spinner_item);
-        //Specify the layout to use when the list of choices appears.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        latest.setAdapter(adapter);
-    }
-
-    //fill type
-    public void populateType() {
-        type = (Spinner) findViewById(R.id.choice);
-        //Create an ArrAdapt using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.type, android.R.layout.simple_spinner_item);
-        //Specify the layout to use when the list of choices appears.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Apply the adapter to the spinner
-        type.setAdapter(adapter);
-    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {}
 }
