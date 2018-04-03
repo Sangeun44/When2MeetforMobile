25dc0432d10c33c1d0eb31c0f8660db9611c3c98
Sangeun44 <eunsang@sas.upenn.edu>
2018-04-03 16:10:21 -0400

    listview

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/SessionDisplayActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/SessionDisplayActivity.java
@@ -6,8 +6,10 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -37,13 +40,15 @@ public class SessionDisplayActivity extends AppCompatActivity {
 
     private FirebaseFirestore database;
 
+    private ListView ls;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_display);
         Intent i = this.getIntent();
         usersInName = new HashMap<String, String>();
-
+        ls = (ListView) findViewById(R.id.listBestTimes);
         meetingID = i.getStringExtra("MEETING"); //meeting_ID
         userID = i.getStringExtra("accountKey"); //owner_ID
 
@@ -82,7 +87,7 @@ public class SessionDisplayActivity extends AppCompatActivity {
         startActivityForResult(i, EnterTimesActivity_ID);
     }
 
-    public void onAddTimesButtonClick (View v) {
+    public void onAddTimesButtonClick(View v) {
         Toast.makeText(getApplicationContext(), "Going to Add Times Page...",
                 Toast.LENGTH_SHORT).show();
         Intent i = new Intent(this, AddTimesActivity.class);
@@ -99,50 +104,50 @@ public class SessionDisplayActivity extends AppCompatActivity {
     private void readSessionData(String meetingID) {
         // get the meeting in the database
         database.collection("meetings").document(meetingID).get()
-            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshots) {
-                    if (documentSnapshots.exists()) {
-                        meeting = documentSnapshots.toObject(Meeting.class);
-                        Log.d(TAG,"onSuccess: Found meeting!");
-                        updateUI(meeting.getUsers(), meeting.getBestTimes());
-                    } else {
-                        Log.d(TAG, "onSuccess: No Such meeting");
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshots) {
+                        if (documentSnapshots.exists()) {
+                            meeting = documentSnapshots.toObject(Meeting.class);
+                            Log.d(TAG, "onSuccess: Found meeting!");
+                            updateUI(meeting.getUsers(), meeting.getBestTimes());
+                        } else {
+                            Log.d(TAG, "onSuccess: No Such meeting");
+                        }
                     }
-                }
-            })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getApplicationContext(), "Error getting data!!!",
-                            Toast.LENGTH_LONG).show();
-                }
-            });
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error getting data!!!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     private void getUserName(final String user_ID) {
-            Log.d(TAG, "user id " + user_ID);
-            database.collection("users").document(user_ID).get()
-                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                        @Override
-                        public void onSuccess(DocumentSnapshot documentSnapshots) {
-                            if (documentSnapshots.exists()) {
-                                String name = documentSnapshots.get("name").toString();
-                                usersInName.put(user_ID, name);
-                                updateUIPeople();
-                                Log.d(TAG,"onSuccess: Found user name!" + user_ID + name);
-                            } else {
-                                Log.d(TAG, "onSuccess: No Such owner");
-                            }
+        Log.d(TAG, "user id " + user_ID);
+        database.collection("users").document(user_ID).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshots) {
+                        if (documentSnapshots.exists()) {
+                            String name = documentSnapshots.get("name").toString();
+                            usersInName.put(user_ID, name);
+                            updateUIPeople();
+                            Log.d(TAG, "onSuccess: Found user name!" + user_ID + name);
+                        } else {
+                            Log.d(TAG, "onSuccess: No Such owner");
                         }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getApplicationContext(), "Error getting data!!!",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error getting data!!!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     /**
@@ -150,7 +155,6 @@ public class SessionDisplayActivity extends AppCompatActivity {
      * Add More Users button, it should display a screen that
      * lists the code for the event
      * with an option to add more usernames/emails.
-     *
      */
     public void addUserButton(View view) {
         Toast.makeText(getApplicationContext(), "Going to Enter More Users Page...",
@@ -172,43 +176,48 @@ public class SessionDisplayActivity extends AppCompatActivity {
         String peopleList = "";
         int counter = 1;
 
-        Log.d(TAG, "string" + usersInName.get("110859387616197731453"));
         for (String id : users.keySet()) {
             User u = users.get(id);
             if (u.enteredTimes()) {
-                peopleList += "\n" + counter +  ". " + usersInName.get(id);
+                peopleList += "\n" + counter + ". " + usersInName.get(id);
                 counter++;
             }
         }
         txtPeople.setText(peopleList);
-
     }
 
     /**
      * Update the UI to reflect the data loaded into {@code meeting}.
-     *
      */
     public void updateUI(Map<String, User> users, Map<Integer, HashSet<String>> allTimes) {
         for (String id : users.keySet()) {
             getUserName(id);
-            Log.d(TAG,"get user name!" + id);
+            Log.d(TAG, "get user name!" + id);
         }
 
-        TextView txtBestTimes = findViewById(R.id.txtBestTimes);
+        //TextView txtBestTimes = findViewById(R.id.txtBestTimes);
         String bestTimes = "";
 
         int numUsers = users.size();
+        List<String> list_times = new ArrayList<String>();
+
+        //final List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
 
         for (int i = numUsers; i > numUsers / 2; i--) {
-                if (allTimes.containsKey(i)) {
-                    String bestTime = (double) i * 100 / numUsers + "% Free:\n";
-                    for (String time : allTimes.get(i)) {
-                        bestTime += time + ":00 \n";
-                    }
-                    bestTimes += bestTime + "\n";
+            if (allTimes.containsKey(i)) {
+                String bestTime = (double) i * 100 / numUsers + "% Free:";
+                list_times.add(bestTime);
+                for (String time : allTimes.get(i)) {
+                    String right = time + ":00";
+                    list_times.add(right);
                 }
             }
-
-            txtBestTimes.setText(bestTimes);
         }
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, list_times);
+        ls.setAdapter(arrayAdapter);
+    }
+
+
 }
