ef42d1db3a90c2dd46d55495687a956db5647c2f
eviebail <ebail@seas.upenn.edu>
2018-04-15 16:38:34 -0400

    added username field

---
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/ProfileActivity.java
@@ -0,0 +1,204 @@
+package edu.upenn.cis350.g8.when2meetformobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Evie on 4/6/18.
+ */
+
+public class ProfileActivity extends AppCompatActivity{
+    public static final int ProfileActivity_ID = 4675047;
+    public static final int GET_FROM_GALLERY = 3;
+    private static Handler mHandler = new Handler();
+    final String TAG = "Profile";
+    private User you;
+    private Bitmap image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getUser();
+    }
+
+    private void getUser() {
+        // get the user from the database
+        String userId = getIntent().getStringExtra("accountId");
+        FirebaseFirestore.getInstance().collection("users").document(userId).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshots) {
+                        if (documentSnapshots.exists()) {
+                            you = documentSnapshots.toObject(User.class);
+
+                            //populate views with what's currently in the db
+                            EditText name = findViewById(R.id.name);
+                            if (you.getUserName() == null) {
+                                name.setText("Username: ");
+                            } else {name.setText(you.getUserName()); }
+
+                            EditText number = findViewById(R.id.phoneNumber);
+                            if (you.getPhoneNumber() == null) {
+                                number.setText("Number: ");
+                            } else {number.setText(you.getPhoneNumber()); }
+
+                            EditText description = findViewById(R.id.description);
+                            if (you.getDescription() == null) {
+                                description.setText("Description: ");
+                            } else {description.setText(you.getDescription()); }
+
+                            if (you.getImage() != null) {
+                                ImageButton img = findViewById(R.id.profileImage);
+                                byte[] decodedString = Base64.decode(you.getImage(), Base64.DEFAULT);
+                                Bitmap decodedByte = BitmapFactory.decodeByteArray
+                                        (decodedString, 0, decodedString.length);
+                                img.setBackground(new BitmapDrawable(getResources(), decodedByte));
+                            }
+                            Log.d(TAG,"onSuccess: Found user!");
+                        } else {
+                            Log.d(TAG, "onSuccess: No Such user");
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error getting data!!!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    protected void onClickSubmit(View view) {
+        EditText name = findViewById(R.id.name);
+        EditText number = findViewById(R.id.phoneNumber);
+        EditText description = findViewById(R.id.description);
+
+        User updatedYou = new User();
+        updatedYou.setUserName(name.getText().toString());
+        updatedYou.setNumber(number.getText().toString());
+        updatedYou.setDescription(description.getText().toString());
+
+        String encoded = "";
+
+        if (image != null) {
+            //save new image to db - Stack exchange!
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream .toByteArray();
+            encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        }
+
+        if (!encoded.isEmpty()) {
+            updatedYou.setImage(encoded);
+        }
+
+        updateDB(updatedYou);
+
+        Toast.makeText(getApplicationContext(), "Profile successfully updated!",
+                Toast.LENGTH_SHORT).show();
+
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, Toast.LENGTH_SHORT + 3000);
+    }
+
+    //Stack exchange
+
+    protected void onImageButtonClick (View view) {
+        startActivityForResult(new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                //set image button bg to image
+                ImageButton profileImage = findViewById(R.id.profileImage);
+                profileImage.setBackground(new BitmapDrawable(getResources(), image));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    protected void onClickCancel(View view) {
+        finish();
+    }
+
+    private void updateDB(User user) {
+        //add to database
+        String userId = getIntent().getStringExtra("accountId");
+
+        List<String> fields = new ArrayList<String>();
+        fields.add("phoneNumber");
+        fields.add("description");
+        fields.add("image");
+        fields.add("userName");
+
+        Log.d(TAG, user.getUserName());
+
+
+        //add new stuff
+        FirebaseFirestore.getInstance().collection("users").document(userId)
+                .set(user, SetOptions.mergeFields(fields))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+    }
+
+}
--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/User.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/User.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class User {
     private String name;               // name of the user
+    private String userName;
+    private String phoneNumber;
+    private String description;
+    private String image;
     private ArrayList<String> myTimes; // availability as list of datetime strings
 
     /**
@@ -22,6 +26,9 @@ public class User {
     public User(String name) {
         this.name = name;
         myTimes = new ArrayList<>();
+        phoneNumber = "";
+        description = "";
+        image = "";
     }
 
     /**
@@ -53,6 +60,30 @@ public class User {
     public String getName() {
         return this.name;
     }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getImage() {return this.image;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setUserName(String name) {this.userName = name;}
+
+    public void setNumber(String number) {phoneNumber = number;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public void setImage(String image) {this.image = image;}
 
     /**
      * Returns true if the user has entered their available times.
