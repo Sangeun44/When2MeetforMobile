0675a7c96eea40ca5ab9743aa5f1ab2e64fb0094
eviebail <ebail@seas.upenn.edu>
2018-04-15 14:36:48 -0400

    implemented changing images

--- app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/ProfileActivity.java
+++ app/src/main/java/edu/upenn/cis350/g8/when2meetformobile/ProfileActivity.java
@@ -1,12 +1,22 @@
 package edu.upenn.cis350.g8.when2meetformobile;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -15,6 +25,9 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +37,11 @@ import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity{
         public static final int ProfileActivity_ID = 4675047;
+        public static final int GET_FROM_GALLERY = 3;
         private static Handler mHandler = new Handler();
         final String TAG = "Profile";
         private User you;
+        private Bitmap image;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,13 @@ public class ProfileActivity extends AppCompatActivity{
             number.setText(you.getPhoneNumber());
             EditText description = findViewById(R.id.description);
             description.setText(you.getDescription());
+
+            if (!you.getImage().isEmpty()) {
+                ImageButton img = findViewById(R.id.profileImage);
+                byte[] decodedString = Base64.decode(you.getImage(), Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                img.setBackground(new BitmapDrawable(getResources(), decodedByte));
+            }
         }
 
     private void getUser() {
@@ -77,6 +99,20 @@ public class ProfileActivity extends AppCompatActivity{
             updatedYou.setNumber(number.getText().toString());
             updatedYou.setDescription(description.getText().toString());
 
+            String encoded = "";
+
+            if (image != null) {
+                //save new image to db - Stack exchange!
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream .toByteArray();
+                encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            }
+
+            if (!encoded.isEmpty()) {
+                updatedYou.setImage(encoded);
+            }
+
             updateDB(updatedYou);
 
             Toast.makeText(getApplicationContext(), "Profile successfully updated!",
@@ -88,6 +124,38 @@ public class ProfileActivity extends AppCompatActivity{
                 }
             }, Toast.LENGTH_SHORT + 3000);
         }
+
+        //Stack exchange
+
+    protected void onImageButtonClick (View view) {
+            startActivityForResult(new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                //set image button bg to image
+                ImageButton profileImage = findViewById(R.id.profileImage);
+                profileImage.setBackground(new BitmapDrawable(getResources(), image));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
         protected void onClickCancel(View view) {
             finish();
